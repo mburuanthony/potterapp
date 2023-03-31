daab153d4b9b357c557d11caec6808257c76d06b
mburuanthony <mburuantony@outlook.com>
2023-03-31 10:33:20 +0300

    home screen

---
+++ screens/HomeScreen.js
@@ -0,0 +1,105 @@
+import { useEffect, useState, useCallback } from "react";
+import { FlatList, StyleSheet, Dimensions, RefreshControl } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Header } from "../components/Header";
+import { Character } from "../components/Character";
+import { usealert } from "../context/alertctx";
+import { getcharacters } from "../api/methods";
+import { container } from "../constants/styles";
+import { colors } from "../constants/colors";
+
+let deviceHeight = Dimensions.get("window").height;
+
+function HomeScreen() {
+  const [characters, setcharacters] = useState([]);
+  const [refreshing, setrefreshing] = useState(false);
+  const { setisvible, setisloading, setissuccess } = usealert();
+  const { lightblack } = colors;
+
+  const onfetchcharacters = async () => {
+    setisvible(true);
+    setisloading(true);
+
+    const { isOK, data } = await getcharacters();
+
+    if (isOK) {
+      setcharacters(data);
+      setisloading(false);
+
+      setissuccess(true);
+
+      setTimeout(() => {
+        setisvible(false);
+        setisloading(false);
+        setissuccess(false);
+      }, 1200);
+    } else {
+      setissuccess(false);
+
+      setTimeout(() => {
+        setisvible(false);
+        setisloading(false);
+        setissuccess(false);
+      }, 1200);
+    }
+
+    setisvible(false);
+    setisloading(false);
+  };
+
+  const onRefresh = useCallback(() => {
+    setrefreshing(true);
+    onfetchcharacters();
+    setrefreshing(false);
+  }, []);
+
+  // flatlist
+  const ITEM_HEIGHT = deviceHeight / 3 - 12;
+  const keyExtractor = (item) => item?.id;
+  const getItemLayout = (data, index) => {
+    return {
+      length: ITEM_HEIGHT,
+      offset: ITEM_HEIGHT * index,
+      index,
+    };
+  };
+  const renderItem = ({ item }) => (
+    <Character charUid={item?.id} charName={item?.name} imgUri={item?.image} />
+  );
+
+  useEffect(() => {
+    onfetchcharacters();
+  }, []);
+
+  return (
+    <SafeAreaView style={container}>
+      <Header />
+      <FlatList
+        style={styles.flatList}
+        showsVerticalScrollIndicator={false}
+        numColumns={2}
+        data={characters}
+        keyExtractor={keyExtractor}
+        maxToRenderPerBatch={6}
+        getItemLayout={getItemLayout}
+        renderItem={renderItem}
+        refreshControl={
+          <RefreshControl
+            refreshing={refreshing}
+            onRefresh={onRefresh}
+            colors={[lightblack]}
+          />
+        }
+      />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  flatList: {
+    alignSelf: "stretch",
+    paddingHorizontal: 8,
+  },
+});
+
+export default HomeScreen;
