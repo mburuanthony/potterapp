625ae5ff636205b482d0e931eb33eade4e8c6a6e
mburuanthony <mburuantony@outlook.com>
2023-03-31 10:33:01 +0300

    constants (styles, colors)

---
+++ constants/colors.js
@@ -0,0 +1,7 @@
+export const colors = {
+  white: "rgba(255, 255, 255, 1)",
+  black: "rgba(0, 0, 0, 1)",
+  blue: "rgba(60, 84, 164, 0.9)",
+  lightblack: "rgba(0, 0, 0, 0.5)",
+  gray: "rgba(217, 217, 217, 0.25)",
+};
---
+++ constants/styles.js
@@ -0,0 +1,17 @@
+import { colors } from "./colors";
+
+const { black, white } = colors;
+
+export const container = {
+  flex: 1,
+  backgroundColor: white,
+};
+
+export const text = {
+  fontSize: 16,
+  color: black,
+};
+
+export const subtitle = {
+  fontSize: 18,
+};
